629c16484f83ceabdcfa1916371b13331230e60a
Rodek Michal <mrodek@corpnet.cz>
2019-09-03 07:46:23 +0200

    promises

--- app.js
+++ app.js
@@ -54,10 +54,10 @@ function search() {
                 //console.log(data)
                 console.log(data._total);
                 let pages = Math.ceil(data._total / 100);
-                //console.log(pages);
+                console.log(pages);
 
                 //console.log(data.streams);
-                for (let i = 1; i < pages; i++) {
+                for (let i = 1; i <= pages; i++) {
                     //console.log(i);
                     fetch(`https://api.twitch.tv/kraken/streams/?broadcaster_language=cs&limit=100&offset=${i * 100}&client_id=u05mtiivnmqdy7ae7ag3fkzheuuiag`)
                         .then(response => response.json()
@@ -65,9 +65,26 @@ function search() {
                                 for (stream of data.streams) {
                                     nicks.push(stream.channel.name);
                                 }
-                                console.log(nicks)
+                                //console.log(nicks);
                             }));
-                }
+                console.log("current i: "+i);
+                        }
+                return nicks;
+            }).then(nicks => {
+                console.log(nicks);
+                const viewerListUrl = nicks.map(nick => `https://tmi.twitch.tv/group/user/${nick}/chatters`);
+                //console.log(viewerListUrl);
+
+                Promise.all(viewerListUrl.map(url => fetch(url)))
+                .then(responses => responses.map(response => response.json()
+                .then(data => {
+                    //console.log(data);
+                    for (viewer of data.chatters.viewers) {
+                        if (viewer == "majrik") {
+                            console.log(viewer);
+                        }
+                    }
+                })));
             })));
 }
 
